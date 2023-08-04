b9d14eb4b36c96fc882c8026161985a788b08991
soyoung <126496661+Forbuds@users.noreply.github.com>
2023-08-04 10:22:28 +0900

    [Bronze II] Title: 수 정렬하기, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,12 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 l = [int(input()) for _ in range(n)]
-# print(l)
-for i in range(n):
-    for j in range(i+1,n):
-        if l[i]>l[j]:
-            c = l[i]
-            l[i] = l[j]
-            l[j] = c
-# print(l)
-l = [str(i) for i in l]
-print('\n'.join(l))
+# print(n,l)
+
+for i in range(n-1):                        # 마지막 인덱스 직전까지 탐색
+    g = l[i]                                # 현재 값 보관
+    c_i = l.index(min(l[i+1:]))             # 가장 작은 인덱스 추출
+    if g <= l[c_i]:
+        pass
+    else:
+        l[i] = l[c_i]
+        l[c_i] = g
+print('\n'.join([str(i) for i in l]))
