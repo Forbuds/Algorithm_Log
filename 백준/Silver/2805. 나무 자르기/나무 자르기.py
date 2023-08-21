5d54897c91d3eee9a479e924353c93af19aa665c
soyoung <126496661+Forbuds@users.noreply.github.com>
2023-08-21 10:48:06 +0900

    [Silver II] Title: 나무 자르기, Time: 4652 ms, Memory: 149680 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,30 @@
+import sys
+import time
+input = sys.stdin.readline
+n,m = map(int, input().strip().split())
+arr = list(map(int, input().strip().split()))
+# m을 하나씩 하기란 불가능에 가까움
+h = max(arr)
+s = 0
+e = max(arr)
+
+flag = 0
+while True:
+    if sum([x-h if x>=h else 0 for x in arr])==m or flag>1:
+        print(h)
+        break
+
+    elif sum([x-h if x>=h else 0 for x in arr])>=m:
+        # print(s, h, e, sum([x-h if x>=h else 0 for x in arr]), m)
+        s = h
+        h = (s + e) // 2
+        if s==h:
+            flag=2
+        # time.sleep(1)
+    elif sum([x-h if x>=h else 0 for x in arr])<=m:
+        # print('',s, h, e, sum([x-h if x>=h else 0 for x in arr]), m)
+        e = h
+        h = (s + e) // 2
+        if e==h:
+            flag=2
+        # time.sleep(1)
