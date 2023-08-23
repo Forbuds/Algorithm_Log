b79408dba13a7b02330f5fc08bbffae4bb12754a
soyoung <126496661+Forbuds@users.noreply.github.com>
2023-08-23 10:18:08 +0900

    [Silver II] Title: 병사 배치하기, Time: 48 ms, Memory: 33320 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -1,12 +1,22 @@
 import sys
+from bisect import bisect_left
+# bisect은 항상 오름차순일때만 사용하자
 input = sys.stdin.readline
 n = int(input())
 arr = list(map(int,input().strip().split()))
-dp = [1 for _ in range(n)]
-
+arr.reverse()
+q = []
+q.append(arr[0])
 for i in range(1,n):
-    for j in range(i):
-        if arr[i] < arr[j]:
-            dp[i] = max(dp[j]+1,dp[i])
-
-print(n-max(dp))
+    x = arr[i]
+    # print(x)
+    if q[-1] < x:
+        q.append(x)
+        # print('크다!',q)
+    else:
+        # print('--------------안크네!',q)
+        idx = bisect_left(q,x)
+        q[idx] = x
+        # print('바꿨어', q)
+# print(q)
+print(n-len(q))
