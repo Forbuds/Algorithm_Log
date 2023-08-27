2f79ec05455a8b76f712f88781da40d3ae7b2cba
soyoung <126496661+Forbuds@users.noreply.github.com>
2023-08-27 20:18:29 +0900

    [Bronze I] Title: 이항 계수 1, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,18 +1,11 @@
 import sys
+from itertools import combinations
 input = sys.stdin.readline
 n,k = map(int, input().strip().split())
 result = 0
-s = []
+s = [i+1 for i in range(n)]
 
-def combi(L,c):
-    global result
-    if L==k:
-        result+=1
-        # print(' '.join(map(str,s)))
-    else:
-        for i in range(c,n):
-            s.append(i+1)
-            combi(L+1,i+1)
-            s.pop()
-combi(0,0)
+# print(combinations(s,k))
+for i in combinations(s,k):
+    result+=1
 print(result)
