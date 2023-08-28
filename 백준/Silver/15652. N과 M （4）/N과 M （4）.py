b8783a624558ea5788cc0c5e369564bfe82dfafd
soyoung <126496661+Forbuds@users.noreply.github.com>
2023-08-28 10:18:21 +0900

    참고 - 발전사항:list 만드는 빠른 방법
    map(str, range(1, N+1)), M

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -6,3 +6,8 @@ n, m = map(int, input().strip().split())
 
 for i in itertools.combinations_with_replacement([x+1 for x in range(n)],m):
     print(' '.join(map(str,i)))
+
+
+# https://www.acmicpc.net/source/65435222
+for i in combinations_with_replacement(map(str, range(1, N+1)), M):
+    print(' '.join(i))
