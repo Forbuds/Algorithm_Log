590836d096b2a8bc42ef83a9dd6d9289653b086f
soyoung <126496661+Forbuds@users.noreply.github.com>
2023-08-28 10:13:55 +0900

    [Silver III] Title: N과 M (4), Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,17 +1,8 @@
 import sys
+import itertools
 input = sys.stdin.readline
 
 n, m = map(int, input().strip().split())
-s = []
 
-def com(l, index):
-    if l == m:
-        print(' '.join(map(str,s)))
-        return
-    else:
-        for i in range(index,n):
-            s.append(i+1)
-            com(l+1,i)
-            s.pop()
-
-com(0,0)
+for i in itertools.combinations_with_replacement([x+1 for x in range(n)],m):
+    print(' '.join(map(str,i)))
