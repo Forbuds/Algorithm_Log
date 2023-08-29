92c16364746a4f3ed3b1c933fa1192281f4c4d80
soyoung <126496661+Forbuds@users.noreply.github.com>
2023-08-29 10:34:51 +0900

    Update 알고리즘 수업 － 깊이 우선 탐색 2.py

--- 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -1,28 +1,32 @@
 import sys
-sys.setrecursionlimit(10**9)
+from collections import deque
+
 input = sys.stdin.readline
 
-
-
-n,m,r = map(int,input().split())
-
-visited = [0]*(n+1)
-g = [[] for i in range(n+1)]
-cnt = 1
-for _ in range(m):
-    u,v = map(int,input().split())
-    g[u].append(v)
-    g[v].append(u)
-
-def dfs(visited,g,r):
-    global cnt
-    visited[r]=cnt
+n,m,r = map(int, input().strip().split())
+g = [[] for _ in range(n+1)]
+v = [0]*(n+1)
+for i in range(m):
+    a,b = map(int, input().strip().split())
+    g[a].append(b)
+    g[b].append(a)
+q = deque([])
+def dfs(r):
+    cnt = 1
+    v[r] = 1
+    q.append(r)
     cnt+=1
-    for r_ in sorted(g[r],reverse=True):
-        if visited[r_]==0:
-            dfs(visited,g,r_)
+    while q:
+        n_c = q.pop()
+        if v[n_c] == 0:
+            v[n_c] = cnt
+            cnt+=1
+        # print(g[n_c])
+        for i in sorted(g[n_c]):
+            if v[i]==0:
+                q.append(i)
 
-dfs(visited,g,r)
-for i in visited[1:]:
-    print(i)
 
+
+dfs(r)
+print(*v[1:],sep='\n')
