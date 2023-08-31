8c61e7c9e6056002cbecad8e21ec89238736c12a
soyoung <126496661+Forbuds@users.noreply.github.com>
2023-08-31 11:04:50 +0900

    [Silver II] Title: 랜선 자르기, Time: 88 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -6,17 +6,19 @@ k, n = map(int,input().strip().split())
 for i in range(k):
     arr.append(int(input().strip()))
 # print(k,n,arr)
-s = 1
+s = 1    # 0으로 시작하면 안됨: 나누기 0 안됨
 e = max(arr)
-while s<=e:
+while s<=e:  # 같아도 멈춰야 함
     mid = (s+e)//2
     result = 0
     for x in arr:
         result += x//mid
     # print(s, mid, e, result)
-    if result >= n:      #mid가 너무 작음
+    if result >= n:      #mid가 너무 작음   # 같아도 해줘야 하는게, 같다 해서 최대길이는 또 아님
         s = mid +1
     else:
         e = mid -1
 
-print(e)
+print(e)   # 최대를 봐야 하기에
+
+
