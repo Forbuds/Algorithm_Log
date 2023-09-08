99b809cae903f31198cca5fc850e81aed1b370ef
soyoung <126496661+Forbuds@users.noreply.github.com>
2023-09-08 19:48:55 +0900

    [level 3] Title: 입국심사, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.py
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.py
@@ -1,28 +1,3 @@
 def solution(n, times):
     answer = 0
-    
-    # 심사를 받는 데 걸리는 시간이 answer
-    s  = 1
-    e = max(times)*n
-    res = e
-    people = 0
-    
-    while s<=e:
-        mid = (s+e)//2
-        people = 0
-        for t in times:
-            people += mid//t
-        if people >= n:  # 시간이 넉넉
-            e = mid -1
-            res = min(res,mid)
-        else:
-            s = mid +1
-            
-    
-    return res
-
-
-
-# mid는 심사를 받는 데에 걸리는 시간
-# mid안에 진짜로 몇명이나 검사받을 수 있는데?
-# 각 타임 mid/타임
+    return answer
