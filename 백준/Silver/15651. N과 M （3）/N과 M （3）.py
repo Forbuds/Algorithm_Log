741ab1832871939cb508e2a667dca1921d098c1f
soyoung <126496661+Forbuds@users.noreply.github.com>
2023-10-30 10:48:15 +0900

    [Silver III] Title: N과 M (3), Time: 1448 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,17 +1,15 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
-
-n,m = map(int,input().strip().split())
-s = deque([])
+n, m = map(int, input().strip().split())
+v = [0]*n
 def dfs(l):
-    if l==m:
-        print(' '.join(map(str, s)))
+    if len(s)==m:
+        print(' '.join(map(str,s)))
         return
     else:
         for i in range(n):
             s.append(i+1)
             dfs(l+1)
             s.pop()
-
+s = []
 dfs(0)
