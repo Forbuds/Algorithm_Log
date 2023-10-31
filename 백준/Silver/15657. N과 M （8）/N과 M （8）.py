24a9b4f79526e6d1ac007be285e6268b1ac8bf43
soyoung <126496661+Forbuds@users.noreply.github.com>
2023-10-31 09:27:12 +0900

    [Silver III] Title: N과 M (8), Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().strip().split())
+arr = list(map(int, input().strip().split()))
+arr = sorted(arr)
+s = []
+def dfs(c,l):
+    if l==m:
+        print(' '.join(map(str,s)))
+        return
+    else:
+        for i in range(c,n):
+            s.append(arr[i])
+            dfs(i,l+1)
+            s.pop()
+
+
+dfs(0,0)
