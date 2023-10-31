fe2805535478a745d1bec6ed879b00617e130d47
soyoung <126496661+Forbuds@users.noreply.github.com>
2023-11-01 00:52:36 +0900

    [Gold V] Title: 신기한 소수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.py
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -6,11 +6,9 @@ def det(x):
     if x <2:
         return False
     for i in range(2,int(x**(0.5))+1):
-        # print(i,x%i)
         if x%i==0:
             return False
     return True
-s = []
 def dfs(l,c):
     # print(f'l : {l}')
     if l==m:
@@ -21,8 +19,5 @@ def dfs(l,c):
             num = (10)*c + x
             # print(num, det(num))
             if det(num):
-                # print(num)
-                s.append(num)
                 dfs(l+1,num)
-                s.pop()
 dfs(0,0)
