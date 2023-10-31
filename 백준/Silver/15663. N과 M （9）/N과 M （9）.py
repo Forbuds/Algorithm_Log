8aea659a1f73a52992bf5755d8c88c244bdb437c
soyoung <126496661+Forbuds@users.noreply.github.com>
2023-10-31 09:34:53 +0900

    [Silver II] Title: N과 M (9), Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,22 +1,21 @@
 import sys
 input = sys.stdin.readline
-
-n,m = map(int, input().strip().split())
-arr = sorted(list(map(int, input().strip().split())))
-v = [0]*n
+n, m = map(int, input().strip().split())
+arr = list(map(int, input().strip().split()))
+arr = sorted(arr)
 s = []
-def dfs(l):
+v = [0]*n
+def dfs(c,l):
     if l==m:
         print(' '.join(map(str,s)))
         return
     else:
-        tmp = -1          
+        tmp = 0
         for i in range(n):
-            if v[i]==0 and tmp!=arr[i]:   #tmp : 같은 레벨에서만 다르면 된다.
-                tmp = arr[i]
+            if v[i]==0 and arr[i]!=tmp:
                 s.append(arr[i])
-                v[i]=1
-                dfs(l+1)
-                s.pop()
+                v[i] = 1
+                dfs(i+1,l+1)
+                tmp = s.pop()
                 v[i] = 0
-dfs(0)
+dfs(0,0)
