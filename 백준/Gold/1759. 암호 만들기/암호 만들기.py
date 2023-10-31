e3be58a2c9751849c2f8b8637723214ea777c367
soyoung <126496661+Forbuds@users.noreply.github.com>
2023-10-31 10:51:47 +0900

    참고 - 발전사항(백트래킹)
    여러가지 풀이법    
    itertools, dfs

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -19,3 +19,39 @@ for i in range(m-3+1):
                 s.append(''.join(sorted(x+y)))
 for x in sorted(s):
     print(x)
+
+
+# 참고 코드: https://www.acmicpc.net/source/68333541
+nCr = itertools.combinations(characters, L)
+mo = ('a', 'e', 'i', 'o', 'u')
+for s in nCr:
+    moCount = 0
+    jaCount = 0
+    for c in s:   # 돌면서, 모음과 자음의 개수를 세고, 조건 충족하면 print해주는 방식
+        if c in mo:
+            moCount += 1
+        else:
+            jaCount += 1
+        if moCount >=1 and jaCount >= 2:
+            print("".join(s))
+            break
+
+# 참고 코드2: https://codinghejow.tistory.com/230
+# dfs 그대로 사용, 조건 달아줄 것
+def dfs(l,c):
+    if l==m:
+        mo,ja = 0,0
+        for x in s:
+            if x in 'aeiou':
+                mo +=1
+            else:
+                ja +=1
+        if mo>=1 and ja >=2:
+            print(''.join(s))
+        return
+    else:
+        for i in range(c,n):
+            s.append(arr[i])
+            dfs(l+1,i+1)
+            s.pop()
+dfs(0,0)
