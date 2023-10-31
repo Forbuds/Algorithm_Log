bbdc2be63f7be186e39776103d99b5cf7ceab2eb
soyoung <126496661+Forbuds@users.noreply.github.com>
2023-10-31 09:38:22 +0900

    [Silver II] Title: N과 M (10), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().strip().split())
+arr = list(map(int, input().strip().split()))
+arr = sorted(arr)
+s = []
+
+def dfs(c,l):
+    if l==m:
+        print(' '.join(map(str, s)))
+        return
+    else:
+        tmp = 0
+        for i in range(c,n):
+            if tmp!=arr[i]:
+                s.append(arr[i])
+                dfs(i+1,l+1)
+                tmp = s.pop()
+dfs(0,0)
