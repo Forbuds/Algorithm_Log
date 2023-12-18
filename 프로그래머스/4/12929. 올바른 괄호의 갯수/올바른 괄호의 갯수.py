16c802fcea8634d15c3df0a6505f208185fe10ee
soyoung <126496661+Forbuds@users.noreply.github.com>
2023-12-18 10:39:20 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
@@ -1,12 +1,26 @@
 from collections import defaultdict
 def solution(n):
-    answer = 0
-    # 카탈란 수 문제
-    # c4 = c0*c3 + c1*c2 + c2*c1 + c3*c0
     dp = defaultdict(int)
     dp[0] = 1
     dp[1] = 1
-    for i in range(2,n+1):
-        for j in range(0,i):
-            dp[i] += dp[j] * dp[i-j-1]
+    if n > 1:
+        for k in range(2,n+1):
+            for i in range(k):
+                dp[k] += dp[i]*dp[k-i-1]
+            
     return dp[n]
+    
+    
+'''
+    예시) N=4 (괄호 4쌍)
+    4보다 작은 괄호 개수에 따른 정답을 dp로 생성 --> 0쌍은 1가지로 정의
+    dp = [1, 1, 2, 5]
+
+    경우 1) ( 한개도 없음 ) x 3쌍 가짓수 ----- dp[0] * dp[3] = 1x5
+    경우 2) ( 1쌍 가짓수 ) x 2쌍 가짓수 ----- dp[1] * dp[2] = 1x2
+    경우 3) ( 2쌍 가짓수 ) x 1쌍 가짓수 ----- dp[2] * dp[1] = 2x1
+    경우 4) ( 3쌍 가짓수 ) x 0쌍 가짓수 ---- dp[3] * dp[0] = 5x1
+
+     ∴ dp[4] = 5 +2 +2 +5 = 14
+ 
+ '''
