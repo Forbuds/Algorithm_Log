1258486863f516fd8b6cf7952fe148d893c9d015
soyoung <126496661+Forbuds@users.noreply.github.com>
2024-01-02 00:37:12 +0900

    Update 41. K 경유지 내 가장 저렴한 항공권.py - TIL 해결

--- Leetcode/4부. 비선형 자료구조/13장. 최단 경로 문제/41. K 경유지 내 가장 저렴한 항공권.py
+++ Leetcode/4부. 비선형 자료구조/13장. 최단 경로 문제/41. K 경유지 내 가장 저렴한 항공권.py
@@ -1,15 +1,26 @@
 class Solution:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
+        import heapq as hq
         g = defaultdict(list)
         for f,t,p in flights:
             g[f].append((t,p))
-        q = [(0,src,k)]
-        while q:
-            cost, node, tmp = heapq.heappop(q)
-            if node == dst:
-                return cost
-            if tmp >= 0:
-                for n_c, dist in g[node]:
-                    heapq.heappush(q,(cost+dist, n_c, tmp-1))
-        return -1
-      
+        INF = int(1e9)
+        distance = [(INF,INF)]*(n)
+        # 경유지 몇 개 가는  중에, 
+
+        def dij(src):
+            q = [(0,0,src)]   # cost, 경유지 몇 개 ,node
+
+            while q:
+                cost, k, node = hq.heappop(q)
+                if node == dst:
+                    return cost
+                if k < distance[node][1]:          # 이 줄과 바로 아래 두 줄은, https://8iggy.tistory.com/115에서와 같이 해당 노드에 방문하기 전보다 경유지가 적어야 한다. - 경유지가 적어야 방문.
+                    distance[node] = (cost, k+1)   #아래에 넣어야 하는줄 알았더니(n_c) node에 해당되는 거였다. 사실 distance가 아니라 visited로 작성해도 됨
+                    if k <= K:
+                        for n_c, dist in g[node]:
+                            dist += cost
+                            hq.heappush(q,(dist, k+1,n_c))
+            return -1
+        result = dij(src)
+        return result
