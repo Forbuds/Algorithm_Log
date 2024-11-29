dcb78df1673c8bbcd0efcc88a1fc85df6b3ccbc6
soyoung <126496661+Forbuds@users.noreply.github.com>
2024-11-29 22:15:14 +0900

    Title: 배열의 특정 위치 찾기 4, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241129/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[10];
+    int tmp;
+    int cnt=0;
+    int s = 0;
+
+    for(int i=0;i<10;i++){
+        scanf("%d",&tmp);
+        if(tmp==0){
+            printf("%d %d",cnt,s);
+        }
+        if(tmp%2==0){
+            cnt+=1;
+            s+=tmp;
+        }
+    }
+    return 0;
+}
