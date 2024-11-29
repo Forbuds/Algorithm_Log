82cd33a70898491d07a81fa6d3b48d4227b08dec
soyoung <126496661+Forbuds@users.noreply.github.com>
2024-11-29 15:57:30 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241129/원소 10개의 합/sum-of-10-elements.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a=0;
+    int k;
+    for(int i=0;i<10;i++){
+        cin>>k;
+        a+=k;
+    }
+    cout<<a;
+    return 0;
+}
