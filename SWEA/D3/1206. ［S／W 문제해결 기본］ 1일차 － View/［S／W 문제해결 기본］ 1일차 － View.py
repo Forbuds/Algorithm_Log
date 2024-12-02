82f1c731afce6a41ae09a42722af64651cbd3f65
soyoung <126496661+Forbuds@users.noreply.github.com>
2024-12-03 00:09:28 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 179 ms, Memory: 62,272 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -1,15 +1,30 @@
-T = 10
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
-    # ///////////////////////////////////////////////////////////////////////////////////
-    '''
+#include<iostream>
 
-        이 부분에 여러분의 알고리즘 구현이 들어갑니다.
+using namespace std;
 
-    '''
-    n = int(input())
-    arr = list(map(int, input().strip().split()))
-    result = 0
-    for i in range(2, n-2):
-        result += max(arr[i]- max(arr[i-2],arr[i-1], arr[i+1],arr[i+2]),0)
-    print(f'#{test_case} {result}')
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T=10;
+
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		int n;
+		int cnt = 0;
+		cin >> n;
+		int arr[1000];
+		for (int i = 0;i < n;i++) {
+			scanf("%d ", &arr[i]);
+		}
+
+		for (int i = 2;i < n-2;i++) 
+		{
+			int maxH = max(max(arr[i - 2], arr[i - 1]), max(arr[i + 1], arr[i + 2]));
+			
+			if (maxH < arr[i]) cnt += (arr[i] - maxH);
+		}
+		printf("#%d %d\n", test_case, cnt);
+
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
