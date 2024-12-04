8974780eef57fbf90ca28df723d3793c664f104e
soyoung <126496661+Forbuds@users.noreply.github.com>
2024-12-04 21:02:51 +0900

    Title: 문자열 범위 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241204/문자열 범위 출력하기/print-string-in-range.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char s[100];
+    scanf("%[^\n]",s);
+
+   for (int i =2;i<10;i++)
+   {
+    printf("%c",s[i]);
+   }
+    return 0;
+}
