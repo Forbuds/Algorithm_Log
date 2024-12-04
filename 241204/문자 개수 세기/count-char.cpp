390d2ba0c1b5f6961cb76b7924b18c597d7a4d05
soyoung <126496661+Forbuds@users.noreply.github.com>
2024-12-04 21:48:13 +0900

    Title: 문자 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241204/문자 개수 세기/count-char.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char s[100], c;
+    int cnt=0,i=0;
+    scanf("%[^\n]", s);
+    cin>>c;
+
+    //printf("%c\n",c);
+    while(1)
+    {
+        //printf("%c",s[i]);
+        if(s[i] == c){
+            cnt+=1;
+            //printf("--");
+        }
+        if(s[i] == '\0'){
+            break;
+        }
+        i++;
+    }
+    printf("%d",cnt);
+    return 0;
+}
