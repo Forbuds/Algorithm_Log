8155049eae69af442a49c69a4a75f717d8e43ace
soyoung <126496661+Forbuds@users.noreply.github.com>
2024-12-04 16:41:28 +0900

    Title: 문자열 두번 출력하기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 241204/문자열 두번 출력하기/print-string-twice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char str[10];
+    scanf("%s",str);
+    printf("%s\n",str);
+    printf("%s",str);
+    return 0;
+}
