d0fc6a6032866f897ba6f3cdd6e5e4385037da1d
soyoung <126496661+Forbuds@users.noreply.github.com>
2024-12-04 17:13:49 +0900

    Title: 문자열의 길이 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241204/문자열의 길이 출력하기/print-strings-length.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    
+    char a[100],b[100];
+
+    scanf("%s",a);
+    scanf("%s",b);
+    printf("%d", strlen(a)+strlen(b));
+
+    return 0;
+}
