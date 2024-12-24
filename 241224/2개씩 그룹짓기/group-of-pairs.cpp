ba5adbaa387fdfd82b2162e348a4e652a1dff058
soyoung <126496661+Forbuds@users.noreply.github.com>
2024-12-24 11:28:29 +0900

    Title: 2개씩 그룹짓기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241224/2개씩 그룹짓기/group-of-pairs.cpp
+++ 241224/2개씩 그룹짓기/group-of-pairs.cpp
@@ -8,18 +8,15 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin>>n;
-    int arr1[n],arr2[n];
+    int arr1[2*n];
 
-    for(int i=0;i<n;i++){
+    for(int i=0;i<2*n;i++){
         cin>>arr1[i];
-    }
-    for(int i=0;i<n;i++){
-        cin>>arr2[i];
     }
 
     sort(arr1, arr1+n,greater<int>());
-    sort(arr2, arr2+n);
-    printf("%d", arr1[n-1]+arr2[n-1]);
+    sort(arr1+n, arr1+2*n);
+    printf("%d", arr1[n-1]+arr1[2*n-1]);
 
     return 0;
 }
