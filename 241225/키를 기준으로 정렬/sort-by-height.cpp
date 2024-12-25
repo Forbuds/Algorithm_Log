0f73ce05e3425aacb3e7c0993e7ebeff1e1dd22f
soyoung <126496661+Forbuds@users.noreply.github.com>
2024-12-25 22:09:53 +0900

    Title: 키를 기준으로 정렬, Time: 17ms, Memory: 2MB, Status: Runtime Error - CodeTree

---
+++ 241225/키를 기준으로 정렬/sort-by-height.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+struct PER{
+    public:
+        string name;
+        int h,w;
+        PER(string name, int h, int w){
+            this->name = name;
+            this->h = h;
+            this->w = w;
+        }
+        PER(){};
+};
+
+bool cmp(const PER &A, const PER &B){
+    return A.h<B.h;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string name;
+    int h,w;
+    int n;
+    cin>>n;
+    PER arr[n];
+
+
+    for(int i=0;i<n;i++){
+        cin>>name>>h>>w;
+        arr[i] = PER(name,h,w);
+    }
+    sort(arr, arr+5, cmp);
+    for(int i=0;i<n;i++){
+        cout<<arr[i].name<<" "<<arr[i].h<<" "<<arr[i].w<<endl;;
+    }
+    return 0;
+}
