e45f7433534759f33fbb1f1b1a64888217935755
soyoung <126496661+Forbuds@users.noreply.github.com>
2025-01-05 21:54:23 +0900

    Title: top k 숫자, Time: 53ms, Memory: 3MB, Status: Passed - CodeTree

--- 250105/top k 숫자/top-k-elements.cpp
+++ 250105/top k 숫자/top-k-elements.cpp
@@ -12,14 +12,14 @@ int main() {
     cin >> n >> k;
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
-        S.insert(arr[i]);
+        S.insert(-arr[i]);
     }
 
     // Write your code here!
-    auto t = S.rbegin();
+    auto t = S.begin();
     for(int i=0;i<k;++i){
-        cout<<*t<<" ";
-        t--;
+        cout<<-*t<<" ";
+        t++;
     }
 
     return 0;
